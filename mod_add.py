48f6e179de532b2a294b359203e5df9b1b5d95e5
Makcmay <101573637+Makcmay@users.noreply.github.com>
2022-11-22 15:56:28 +0300

    add creat file

--- mod_add.py
+++ mod_add.py
@@ -1,7 +1,6 @@
 import view as v
 import csv
-
-
+import os.path
 
 def write_data():
     name = v.correct_name('Введите Имя : ')
@@ -9,10 +8,38 @@ def write_data():
     sec_name = v.correct_name('Введите Отчество : ')
     phone = v.correct_number()
 
-    with open('data.csv', 'a+', encoding='utf-8', newline='') as csvfile:
-        fieldname = ['first_name', 'second_name', 'last_name', 'phone']
-        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldname)
+    if os.path.exists('data.csv'):
 
-        # csv_writer.writeheader()
-        csv_writer.writerow({'last_name': surname,
-        'first_name': name, 'second_name': sec_name, 'phone': phone})
+        with open('data.csv', 'r', encoding='utf-8') as csvfile2:
+            csv_reader_2 = csv.DictReader(csvfile2)
+            max_id = 1
+            for row in csv_reader_2:
+                if max_id < int(row['id']):
+                    max_id = int(row['id'])
+
+        with open('data.csv', 'a+', encoding='utf-8', newline='') as csvfile3:
+            fieldname_3 = ['id', 'first_name', 'second_name', 'last_name', 'phone']
+            csv_writer = csv.DictWriter(csvfile3, fieldnames=fieldname_3)
+
+            csv_writer.writerow({'id': max_id+1, 'last_name': surname,
+                                'first_name': name, 'second_name': sec_name, 'phone': phone})
+            
+    else:
+        with open('data.csv', 'w', encoding='utf-8', newline='') as csvfile3:
+            fieldname_3 = ['id', 'first_name', 'second_name', 'last_name', 'phone']
+            csv_writer = csv.DictWriter(csvfile3, fieldnames=fieldname_3)
+            csv_writer.writeheader()
+
+        with open('data.csv', 'r', encoding='utf-8') as csvfile2:
+            csv_reader_2 = csv.DictReader(csvfile2)
+            max_id = 1
+            for row in csv_reader_2:
+                if max_id < int(row['id']):
+                    max_id = int(row['id'])
+
+        with open('data.csv', 'a+', encoding='utf-8', newline='') as csvfile3:
+            fieldname_3 = ['id', 'first_name', 'second_name', 'last_name', 'phone']
+            csv_writer = csv.DictWriter(csvfile3, fieldnames=fieldname_3)
+
+            csv_writer.writerow({'id': max_id+1, 'last_name': surname,
+                                'first_name': name, 'second_name': sec_name, 'phone': phone})
