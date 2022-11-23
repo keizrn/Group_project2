faa3e5d22e28be42541840f4005fc7ecd415d3a1
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-11-23 09:48:59 +0300

    Add mod_changes

---
+++ mod_changes.py
@@ -0,0 +1,37 @@
+import csv
+
+
+def data_changes():
+    with open('data.csv', 'r', encoding='utf-8', newline='') as csvfile3:
+        fieldname_3 = ['id', 'first_name', 'second_name', 'last_name', 'phone']
+        csv_reader_3 = csv.DictReader(csvfile3)
+        rows = list(csv_reader_3)
+
+    print('Выбор параметра для поиска контакта.')
+    for i in range (len(rows[0])):
+        print(f'{i} - {rows[0][i]}')
+    identificator_col = int('Введите номер выбранного параметра.')
+    identificator = input('Введите данные для поиска.')
+    
+    for i in rows:
+        if i[identificator_col] == identificator:
+            isFound = True
+            print('Выбор параметра для внесения изменений.')
+            for j in range (len(rows[0])):
+                print(f'{j} - {rows[0][j]}')
+            modifying_col = int(input('Введите номер выбранного параметра.'))
+            print(f'Текущие данные: {i[modifying_col]}')
+            new_value = input(f'Введите новые данные.')
+            old_value = i[modifying_col]
+            i[modifying_col] = new_value
+        else:
+            isFound = False
+    
+    with open('data.csv', 'w', encoding='utf-8', newline='') as csvfile3:
+        fieldname_3 = ['id', 'first_name', 'second_name', 'last_name', 'phone']
+        csv_writer = csv.DictWriter(csvfile3, fieldnames=fieldname_3)
+        csv_writer.writerow(rows)
+    if isFound:
+        return ('Изменения внесены.')
+    else:
+        return ('Запись не найдена.')
