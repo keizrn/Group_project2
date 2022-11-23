d1c1cfa16fdfb4b1f26e43e0303f896abb6ea29c
keizrn <111270948+keizrn@users.noreply.github.com>
2022-11-23 16:42:08 +0300

    Mod Search. 23.11.22 16:40

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 import mod_add as ma
 # import mod_get_bd as mg
 import mod_browse as mb
+import mod_search as ms
 import view as v
 def push_the_button():
     login = v.correct_name('Введите ваш Login : ')
@@ -9,3 +10,5 @@ def push_the_button():
         print(ma.write_data())
     elif info == 2:
         print(mb.main_browse())
+    elif info == 3:
+        print(ms.search_data(input('Введите поисковый запрос(точный!): -> ')))
--- mod_search.py
+++ mod_search.py
@@ -1,13 +1,12 @@
 import csv
 
-def search_data(last_name):
+def search_data(search_q):
     with open('data.csv', 'r', encoding='utf-8') as csvfile_4:
-        reader = csv.DictReader(csvfile_4)
+        reader_search = csv.reader(csvfile_4)
         is_found = False
-        for row in reader:
-            # print(row[2])
-            if row[2] == last_name:
-                return print(f' {row[0]} - {row[1]} {row[2]} {row[3]} {row[4]} {row[5]}')
+        for row in reader_search:
+            if search_q in row:
+                print(row)
                 is_found = True
                 break
         if not is_found:
